a3268b59d8c1f36493df44ce3b29a8623723a3b2
Tom de Bruin <tom@deadlyhifi.com>
2019-12-18 15:37:02 +0000

    A bit of interaction

--- src/webComponent.ts
+++ src/webComponent.ts
@@ -1,9 +1,27 @@
-import { LitElement, html } from "lit-element";
+import { LitElement, html, property } from "lit-element";
+
+enum ButtonLabel {
+  Enable = "Enable",
+  Disable = "Disable"
+}
 
 class customHTMLElement extends LitElement {
+  @property({ type: String }) text = "Hello World";
+  @property({ type: ButtonLabel }) buttonLabel = ButtonLabel.Enable;
+  @property({ type: Boolean }) disabled = false;
+
+  clickHandler(event: Event) {
+    event.preventDefault();
+    this.disabled = !this.disabled;
+    this.buttonLabel = this.disabled ? ButtonLabel.Enable : ButtonLabel.Disable;
+  }
+
   render() {
     return html`
       <p>Hello world! From my-custom-component</p>
+      <p>${this.text}</p>
+      <input type="text" ?disabled="${this.disabled}" />
+      <button @click="${this.clickHandler}">${this.buttonLabel}</button>
     `;
   }
 }
